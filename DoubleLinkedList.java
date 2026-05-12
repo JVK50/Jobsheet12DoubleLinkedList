19140b7235b3c1f5922fd9a034b051fdacfb654d
JVK50 <jidanali32@gmail.com>
2026-05-12 15:04:51 +0700

    Modifikasi

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -86,6 +86,34 @@ public void printReverse() {
     }
 }
 
+// // ---- PERCOBAAN 2: removeFirst() ----
+// public void removeFirst() {
+//     if (isEmpty()) {
+//         System.out.println("Linked List kosong.");
+//         return;
+//     }
+//     if (head == tail) {
+//         head = tail = null;
+//     } else {
+//         head = head.next;
+//         head.prev = null;
+//     }
+// }
+
+// // ---- PERCOBAAN 2: removeLast() ----
+// public void removeLast() {
+//     if (isEmpty()) {
+//         System.out.println("Linked List kosong.");
+//         return;
+//     }
+//     if (head == tail) {
+//         head = tail = null;
+//     } else {
+//         tail = tail.prev;
+//         tail.next = null;
+//     }
+// }
+
 // ---- PERCOBAAN 2: removeFirst() ----
 public void removeFirst() {
     if (isEmpty()) {
